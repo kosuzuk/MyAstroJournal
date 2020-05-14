b2a10e99d8825302c7d5933c4efb19fa08822e02
Koso Suzuki <therealkoso@gmail.com>
2020-05-13 17:45:32 -0700

    a

---
+++ My Astro Journal/MonthlyChallengeEntryCell.swift
@@ -0,0 +1,12 @@
+//
+//  MonthlyChallengeEntryCell.swift
+//  My Astro Journal
+//
+//  Created by Koso Suzuki on 5/13/20.
+//  Copyright © 2020 Koso Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class MonthlyChallengeEntryCell: UITableViewCell {
+}
---
+++ My Astro Journal/MonthlyChallengeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MonthlyChallengeViewController.swift
+//  My Astro Journal
+//
+//  Created by Koso Suzuki on 5/13/20.
+//  Copyright © 2020 Koso Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class MonthlyChallengeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var entriesTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MonthlyChallengeEntryCell
+        return cell
+    }
+}
+
